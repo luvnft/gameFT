0d950b45599a7ee8d02d713af8c23c9a65a8149d
luv nft <info@blkluv.org>
2023-12-09 09:47:49 -0500

    updated

--- app/layout.js
+++ app/layout.js
@@ -4,9 +4,9 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "GameFT-6551",
+  title: "GameFI",
   description:
-    "GameFT-6551: Transforming gaming with ERC-6551 NFTs. Play, collect, and unlock unique features. Join the future of GameFi now!",
+    "GameFI: Transforming gaming with ERC-6551 NFTs. Play, collect, and unlock unique features. Join the future of GameFi now!",
 };
 
 export default function RootLayout({ children }) {
